37aeb498d6eba1eb56df0a2fbc2184f21eeb655a
Suraj Kumar Yadav <thesuraj396@gmail.com>
2024-11-04 23:27:12 +0530

    Submission

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import KanbanBoard from './KanbanBoard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>KANBAN BOARD</h1>
+      <KanbanBoard />
+    </div>
+  );
+}
+
+export default App;
